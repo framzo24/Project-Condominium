73f7c7c73d6eca999eb6ba4ce1afae7f801b3c7b
framzo24 <77861676+framzo24@users.noreply.github.com>
2023-06-08 14:42:37 +0200

    iniziato database

---
+++ Database.sql
@@ -0,0 +1,14 @@
+DROP DATABASE IF EXISTS database_condominium;
+
+USE database_condominium;
+
+CREATE TABLE Persona {
+    name,
+    surname,
+    email,
+    age,
+}
+
+CREATE TABLE FamilyMember {
+    
+}
