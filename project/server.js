b2454276588150dc0a8c0e24c2fd08ce5f61c105
framzo24 <77861676+framzo24@users.noreply.github.com>
2023-07-11 21:52:55 +0200

    Pagina di login

--- project/server.js
+++ project/server.js
@@ -24,7 +24,11 @@ app.get('/', (req, res) => {
 });
   
 app.get('/about', (req, res) => {
-    res.send('Pagina about');
+    res.sendFile('/Users/francescozoni/Documents/UniPR/Tecnologie Internet/Project-Condominium/project/index.html');
+});
+
+app.get('/login', (req, res) => {
+    res.sendFile('/Users/francescozoni/Documents/UniPR/Tecnologie Internet/Project-Condominium/project/login.html');
 });
 
 app.get('/users', (req, res) => {
@@ -42,4 +46,19 @@ app.listen(port, () => {
     console.log(`Server in ascolto sulla porta ${port}`);
 });
 
-  
+app.post('/login', (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  // Query per il controllo delle credenziali nel database
+  const query = "SELECT * FROM Condomini WHERE email = "+username+" AND password = "+password;
+  connection.query(query, (err, results) => {
+    if (err) {
+      res.status(500).send('Errore del server');
+    } else if (results.length === 0) {
+      res.status(401).send('Credenziali non valide');
+    } else {
+      res.status(200).send('Login effettuato con successo');
+    }
+  });
+});
